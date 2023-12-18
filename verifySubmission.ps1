c83f1a76ed01e2ec6b82c71045ff8af88ebb93c6
Dmitry <dgridas@gmail.com>
2023-12-18 22:51:59 +0200

    -added description of implementation

--- verifySubmission.ps1
+++ verifySubmission.ps1
@@ -1,24 +1,12 @@
 
 # feel free to modify this line if your project structure is different to expected
-cd VismaChallenge/Employee
+cd HdrBoxReader
 
-$Env:connectionString="Server=localhost; User ID=postgres; Password=guest; Port=7777; Database=analyticsproject;"
+$Env:connectionString="Server=localhost; User ID=postgres; Password=guest; Port=7777; Database=hdrboxstorage;"
 
 dotnet build
 
-dotnet run --no-build --project .\set-employee\set-employee.csproj --employeeId 1 --employeeName John --employeeSalary 333
-dotnet run --no-build --project .\set-employee\set-employee.csproj --employeeId 2 --employeeName Mary --employeeSalary 444
+dotnet run --no-build --project .\HdrBoxReader\HdrBoxReader.csproj
 
-dotnet run --no-build --project .\get-employee\get-employee.csproj --employeeId 1
-dotnet run --no-build --project .\get-employee\get-employee.csproj --employeeId 2
-
-$past = (Get-Date).ToUniversalTime()
-
-Start-Sleep -s 2
-
-dotnet run --no-build --project .\set-employee\set-employee.csproj --employeeId 1 --employeeName John --employeeSalary 555
-
-dotnet run --no-build  --project .\get-employee\get-employee.csproj --employeeId 1 
-dotnet run --no-build  --project .\get-employee\get-employee.csproj --employeeId 1 --simulatedTimeUtc "$($past.ToString("yyyy-MM-ddTHH:mm:ssZ"))"
 
 cd ../..
